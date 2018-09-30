182f71e8b3184d4167d8af83bcfe9f4bad1eb6db
Андреј Јанчевски <bani57@rocketmail.com>
2018-10-01 00:07:30 +0200

    [backend] Implement GET requests for Following.

---
+++ backend/api/v1/finki_blog/followings/getFollowers.php
@@ -0,0 +1,26 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: access");
+header("Access-Control-Allow-Methods: GET");
+header("Access-Control-Allow-Credentials: true");
+header('Content-Type: application/json');
+
+include_once '../config/database.php';
+
+
+$database = new Database();
+$db = $database->getConnection();
+
+$user = isset($_GET['user']) ? $_GET['user'] : die();
+
+$q = $db->prepare("SELECT F.user1 AS user, F.sinceDate AS sinceDate FROM Following F WHERE F.user2 = :user ORDER BY F.sinceDate DESC");
+$q->bindParam(":user", $user, PDO::PARAM_STR, 30);
+
+
+if ($q->execute()) {
+    echo json_encode($q->fetchAll(PDO::FETCH_ASSOC));
+} else {
+    echo json_encode(
+   array('message' => 'Error while getting followed users.', 'error' => $q->errorInfo() )
+);
+}
