e1824294118743a5d265dbd08b0d35e3a33bdf1c
Андреј Јанчевски <bani57@rocketmail.com>
2018-09-30 23:57:31 +0200

    [backend] Add missing DELETE method declaration to delete requests.

--- backend/api/v1/finki_blog/comments/deleteComment.php
+++ backend/api/v1/finki_blog/comments/deleteComment.php
@@ -1,6 +1,7 @@
 <?php
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: access");
+header("Access-Control-Allow-Methods: DELETE");
 header("Access-Control-Allow-Credentials: true");
 header('Content-Type: application/json');
 
