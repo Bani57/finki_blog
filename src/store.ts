8e3e23566f6a3ee7822d77422b38186da5a8cc06
Андреј Јанчевски <bani57@rocketmail.com>
2018-10-04 02:09:52 +0200

    [frontend] Prepare for deployment to production: migrate requests to https, fix lint errors, refactor code

--- src/store.ts
+++ src/store.ts
@@ -1,32 +1,31 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-import toastr from 'toastr';
+import toastr from 'toastr'
 import 'toastr/build/toastr.css'
 
 // Mutations
-const SET_ALL_USERS = 'SET_ALL_USERS';
-const SET_CURRENT_USER = 'SET_CURRENT_USER';
+const SET_ALL_USERS = 'SET_ALL_USERS'
+const SET_CURRENT_USER = 'SET_CURRENT_USER'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
     allUsers: {},
-    currentUser: null,
+    currentUser: null
   },
   mutations: {
-    [SET_ALL_USERS](state, users) {
-      for (var user in users)
-        Vue.set(state.allUsers, users[user].username, users[user]);
+    [SET_ALL_USERS] (state, users) {
+      for (var user in users) { Vue.set(state.allUsers, users[user].username, users[user]) }
     },
-    [SET_CURRENT_USER](state, user) {
-      state.currentUser = user;
-    },
+    [SET_CURRENT_USER] (state, user) {
+      state.currentUser = user
+    }
   },
   actions: {
-    getAllUsers({ commit, dispatch, state }) {
-      return fetch(`http://${process.env.VUE_APP_HOST}:8080${process.env.BASE_URL}${process.env.VUE_APP_API}/users/getAllUsers.php`, {
-        //credentials: 'include'
+    getAllUsers ({ commit, dispatch, state }) {
+      return fetch(`https://${process.env.VUE_APP_HOST}${process.env.BASE_URL}${process.env.VUE_APP_API}/users/getAllUsers.php`, {
+        credentials: 'include'
       }).then(response => {
         if (response.ok) {
           return response.json()
@@ -34,21 +33,18 @@ export default new Vuex.Store({
           return Promise.reject(new Error('Failed getting users'))
         }
       }, reason => {
-        toastr.options.preventDuplicates = true;
+        toastr.options.preventDuplicates = true
         toastr.error('Unable to fetch users. Try reloading', 'ERROR')
         return Promise.reject(reason)
       }).then(data => {
-        if (data)
-          commit(SET_ALL_USERS, data);
+        if (data) { commit(SET_ALL_USERS, data) }
         return data
       })
     },
-    setCurrentUser({ commit, dispatch, state }, username) {
-      if (state.allUsers[username])
-        commit(SET_CURRENT_USER, state.allUsers[username]);
-      else {
-        fetch(`http://${process.env.VUE_APP_HOST}:8080${process.env.BASE_URL}${process.env.VUE_APP_API}/users/getUserByUsername.php?username=${username}`, {
-          //credentials: 'include'
+    setCurrentUser ({ commit, dispatch, state }, username) {
+      if (state.allUsers[username]) { commit(SET_CURRENT_USER, state.allUsers[username]) } else {
+        fetch(`https://${process.env.VUE_APP_HOST}${process.env.BASE_URL}${process.env.VUE_APP_API}/users/getUserByUsername.php?username=${username}`, {
+          credentials: 'include'
         }).then(response => {
           if (response.ok) {
             return response.json()
